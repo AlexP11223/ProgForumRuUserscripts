11c180207a951b4d82379e4bbbeddfacd445a6e1
Alex P <alex.pantec@gmail.com>
2016-09-23 20:33:30 +0300

    fix youtube video url detection

--- pf_video_embed.user.js
+++ pf_video_embed.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ProgrammersForumVideoEmbed
 // @namespace    http://programmersforum.ru/
-// @version      0.2
+// @version      0.21
 // @description  replaces youtube and coub links with embedded video player frames
 // @author       Alex P
 // @include      http://programmersforum.ru/*
@@ -34,7 +34,7 @@
         var url = $(link).attr('href');
 
         var youtubeIdMatch = url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
-        if(youtubeIdMatch) {
+        if(youtubeIdMatch && youtubeIdMatch[1].indexOf('/') == -1) { // quick fix, should improve youtube detection, for example like here https://github.com/honestbleeps/Reddit-Enhancement-Suite/blob/b0190f3e2ab0887ced5529353394cb43553a113f/lib/modules/hosts/youtube.js#L8
             insertEmbed($(link), YOUTUBE_EMBED_TEMPLATE, youtubeIdMatch[1]);
         }
 
