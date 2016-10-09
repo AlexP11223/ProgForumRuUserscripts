f6e12b0c65ea4ee0d402c399089c2933aaccc56d
Alex P <alex.pantec@gmail.com>
2016-10-09 19:06:15 +0300

    non-userscript scripts

---
+++ non-user-js/quick_quote.js
@@ -0,0 +1,151 @@
+var quickquote = new function() {
+    'use strict';
+
+    var currSelectedText = '';
+    var currPostId = '';
+    var currAuthorName = '';
+
+    var qqBtnId = 'quick_quote_btn';
+    var qqBtnSelector = '#' + qqBtnId;
+    var qqBtn = undefined;
+
+    var fadeOutTimer = undefined;
+
+    function getSelectedText() {
+        return window.getSelection().toString();
+    }
+
+    function onPostClicked(e) {
+        var selectedText = $.trim(getSelectedText());
+        if (selectedText) {
+            stopFadeOut();
+
+            qqBtn.css({ top: (e.pageY + 10) + 'px', left: e.pageX + 'px'});
+
+            qqBtn.show(50, function() {
+                restartFadeOut();
+            });
+
+            var postContainer = $(this).closest('table');
+
+            currPostId = postContainer.attr('id').replace('post', '');
+            currAuthorName = postContainer.find('.bigusername').first().text();
+
+            currSelectedText = selectedText;
+        }
+        else {
+            qqBtn.hide();
+        }
+    }
+
+    function appendText(text) {
+        if (vB_Editor[QR_EditorID].get_editor_contents().length > 0) {
+            text = '\n' + text;
+        }
+        vB_Editor[QR_EditorID].insert_text(text);
+        vB_Editor[QR_EditorID].collapse_selection_end();
+    }
+
+    function appendQuote(text) {
+        var nameQuoteChar = '';
+        if (currAuthorName.indexOf(']') !== -1)
+            nameQuoteChar = "'";
+
+        appendText('[QUOTE=' + nameQuoteChar + currAuthorName + ';' + currPostId + nameQuoteChar + ']' + text + '[/QUOTE]\n');
+    }
+
+    function quoteSelected() {
+        appendQuote(currSelectedText);
+
+        qqBtn.hide();
+    }
+
+    function quotePost(postQuoteUrl, progressIndicator) {
+        progressIndicator.show();
+
+        $.get(postQuoteUrl, function(response) {
+            var html = $.parseHTML(response);
+
+            var quote = $.trim($(html).find('#vB_Editor_001_textarea').text());
+
+            if (quote) {
+                appendText(quote);
+            }
+        }).done(function() {
+            progressIndicator.hide();
+        });
+    }
+
+    function stopFadeOut() {
+        if (fadeOutTimer) {
+            clearTimeout(fadeOutTimer);
+        }
+        qqBtn.stop(true, true);
+    }
+
+    function restartFadeOut() {
+        stopFadeOut();
+
+        fadeOutTimer = setTimeout(function() {
+            qqBtn.fadeOut();
+        }, 3000);
+    }
+
+    function doInit(options) {
+        if (options.selection) {
+            $('<div id="' + qqBtnId + '" class="smallfont qq-btn" style="display:none;">Цитировать</div>').prependTo($('body'));
+
+            qqBtn = $(qqBtnSelector);
+
+            qqBtn.click(quoteSelected);
+
+            qqBtn.hover(function () {
+                    stopFadeOut();
+                },
+                function () {
+                    restartFadeOut();
+                });
+
+            $('#posts').on('mouseup', 'div[id^="post_message"]', onPostClicked);
+        }
+
+        if (options.post) {
+            $('#posts').find('a:has(img[src*="quote."])').click( function(e) {
+                e.preventDefault();
+
+                var url = $(this).attr('href');
+                var progressIndicator = $(this).prevAll('img[id^="progress"]').first();
+
+                quotePost(url, progressIndicator);
+            });
+        }
+    }
+
+    this.init = function(options) {
+        var defaultOptions = {
+            selection: true,
+            post: true
+        };
+
+        if (options === undefined) {
+            options = defaultOptions;
+        } else {
+            options = $.extend(defaultOptions, options);
+        }
+
+        try {
+            if (vB_Editor[QR_EditorID]) { }
+        } catch (e) {
+            // not logged in
+            return;
+        }
+
+        if (window.quickQuoteInitialized || $(qqBtnSelector).length)
+            return;
+        window.quickQuoteInitialized = true;
+
+        $(function() {
+            doInit(options);
+        });
+    }
+};
---
+++ non-user-js/video_embed.js
@@ -0,0 +1,178 @@
+var videoembed = new function() {
+    'use strict';
+
+    var self = this;
+
+    var YOUTUBE_EMBED_TEMPLATE = '<iframe src="https://www.youtube.com/embed/_ID_" width="560" height="315" frameborder="0" allowfullscreen></iframe>';
+    var COUB_EMBED_TEMPLATE = '<iframe src="//coub.com/embed/_ID_?muted=false&autostart=false&originalSize=false&startWithHD=true" width="640" height="270" frameborder="0" allowfullscreen></iframe>';
+
+    function insertEmbed(origLinkNode, embedTemplate, id, hide) {
+        var html = '<br/>' + embedTemplate.replace('_ID_', id) + '<br/>';
+
+        $(html).insertAfter(origLinkNode);
+        origLinkNode.addClass('_embedded');
+
+        if (hide) {
+            origLinkNode.hide();
+        }
+    }
+
+    function parseURL(url) {
+        var parser = document.createElement('a');
+        var searchDict = {};
+
+        parser.href = url;
+
+        var queries = parser.search.replace(/^\?/, '').split('&');
+        var i;
+        for (i = 0; i < queries.length; i++) {
+            var parts = queries[i].split('=');
+            searchDict[parts[0]] = parts[1];
+        }
+
+        return {
+            protocol: parser.protocol,
+            host: parser.host,
+            hostname: parser.hostname,
+            port: parser.port,
+            pathname: parser.pathname,
+            search: parser.search,
+            hash: parser.hash,
+            searchDict: searchDict,
+            pathParts: parser.pathname.substring(1).split('/')
+        };
+    }
+
+    function containsAny(str, substrings) {
+        for (var i = 0; i != substrings.length; i++) {
+            var substring = substrings[i];
+            if (str.indexOf(substring) != -1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    this.parseYoutube = function(href) {
+        function parseId(url) {
+            if (url.hostname.indexOf('youtu.be') != -1)
+                return url.pathParts[0];
+
+            if (url.searchDict['v'])
+                return url.searchDict['v'];
+
+            if (['watch', 'embed', 'v'].indexOf(url.pathParts[0]) != -1)
+                return url.pathParts[1];
+
+            return false;
+        }
+
+        function parseTime(s) {
+            try {
+                var parts = s.replace(/[^0-9]+/g, ' ').split(' ');
+                parts = $.grep(parts, function(el) { return el !== '';});
+
+                if (parts.length === 1) {
+                    return parseInt(parts[0], 10);
+                } else if (parts.length === 2) {
+                    return parseInt(parts[0], 10) * 60 + parseInt(parts[1], 10);
+                } else if (parts.length === 3) {
+                    return parseInt(parts[0], 10) * 3600 + parseInt(parts[1], 10) * 60 + parseInt(parts[2], 10);
+                }
+            } catch (e) {
+            }
+            return 0;
+        }
+
+        var url = parseURL(href);
+
+        var domains = ['youtube.com', 'youtu.be'];
+
+        if (!containsAny(url.hostname, domains))
+            return false;
+
+        var id = parseId(url);
+
+        if (id) {
+            var ret = { id: id, params: { } };
+
+            if (url.searchDict['t']) {
+                var t = parseTime(url.searchDict['t']);
+                if (t) {
+                    ret.params.start = t;
+                }
+            }
+
+            return ret;
+        }
+
+        return false;
+    };
+
+    this.parseCoubId = function(href) {
+        var url = parseURL(href);
+
+        var domains = ['coub.com'];
+
+        if (!containsAny(url.hostname, domains))
+            return false;
+
+        if (['view', 'embed'].indexOf(url.pathParts[0]) != -1)
+            return url.pathParts[1];
+
+        return false;
+    };
+
+    function doInit(options) {
+        var postBlocks = $('#posts, #post, td.alt1:has(hr)');
+
+        $.each(postBlocks.find('a[href*="youtu"], a[href*="coub"]'), function (i, link) {
+            if (!$(link).is(':visible') || $(link).is('._embedded'))
+                return;
+
+            // skip signature
+            if ($(link).parents('div').eq(0).is(':contains("__________________")'))
+                return;
+
+            var url = $(link).attr('href');
+
+            var youtube = self.parseYoutube(url);
+            if (youtube) {
+                var paramsStr = '';
+                if (!$.isEmptyObject(youtube.params)) {
+                    paramsStr = '?' + $.param(youtube.params);
+                }
+                insertEmbed($(link), YOUTUBE_EMBED_TEMPLATE, youtube.id + paramsStr, options.hideLinks);
+            }
+
+            if (options.coub) {
+                var coubId = self.parseCoubId(url);
+                if (coubId) {
+                    insertEmbed($(link), COUB_EMBED_TEMPLATE, coubId, options.hideLinks);
+                }
+            }
+        });
+    }
+
+    this.init = function(options) {
+        var defaultOptions = {
+            youtube: true,
+            coub: true,
+            hideLinks: false
+        };
+
+        if (options === undefined) {
+            options = defaultOptions;
+        } else {
+            options = $.extend(defaultOptions, options);
+        }
+
+        if (window.videoEmbedInitialized)
+            return;
+        window.videoEmbedInitialized = true;
+
+        $(function() {
+            doInit(options);
+        });
+    };
+};
