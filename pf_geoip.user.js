ebc4c26b75fc35ab15a191844e131836f19b596f
Alex P <alex.pantec@gmail.com>
2017-02-13 09:44:03 +0200

    geoip

---
+++ pf_geoip.user.js
@@ -0,0 +1,95 @@
+// ==UserScript==
+// @name         ProgrammersForumGeoIp
+// @namespace    http://programmersforum.ru/
+// @version      0.1
+// @description  adds country/city info on the page with user IP
+// @author       Alex P
+// @include      http://programmersforum.ru/postings.php?do=getip*
+// @include      http://www.programmersforum.ru/postings.php?do=getip*
+// @grant        none
+// @downloadURL  https://github.com/AlexP11223/ProgForumRuUserscripts/raw/master/pf_geoip.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function getJson(url, success, error) {
+        const request = new XMLHttpRequest();
+        request.open('GET', url, true);
+
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                const data = JSON.parse(request.responseText);
+                success(data);
+            } else {
+                error(`HTTP error ${request.status} ${request.statusText}`);
+            }
+        };
+
+        request.onerror = function() {
+            error('Connection error');
+        };
+
+        request.send();
+    }
+
+    function requestIpApi(ip, success, error) {
+        getJson(`http://ip-api.com/json/${ip}`, function (data) {
+                if (data.status === 'success') {
+                    success({ country: data.country, region: data.regionName, city: data.city, isp: data.isp });
+                } else {
+                    error(`API error ${data.message}`);
+                }
+            },
+            error);
+    }
+
+    function requestFreegeoip(ip, success, error) {
+        getJson(`http://freegeoip.net/json/${ip}`, function (data) {
+                success({ country: data.country_name, region: data.region_name, city: data.city });
+            },
+            error);
+    }
+
+    function formatGeoipData(data) {
+        let result = `${data.country}, ${data.region}, ${data.city}`;
+        if (data.isp) {
+            result += ` (провайдер ${data.isp})`;
+        }
+        return result;
+    }
+
+    function formatError(error) {
+        return `<span style="color: red;">${error}</span>`;
+    }
+
+    const ipElement = window.document.querySelector('.panelsurround div.panel div div strong');
+    if (!ipElement)
+        return;
+
+    const ip = ipElement.innerText;
+
+    const container = ipElement.parentNode;
+
+    function appendLine(name, content) {
+        function elementFromString(html) {
+            const range = document.createRange();
+            return range.createContextualFragment(html);
+        }
+
+        container.appendChild(elementFromString(`<br>${name}: `));
+        container.appendChild(elementFromString(`<strong>${content}</strong>`));
+    }
+
+    requestIpApi(ip, function (data) {
+            appendLine('Месторасположение (ip-api.com)', formatGeoipData(data));
+    }, function (error) {
+        appendLine('Месторасположение (ip-api.com)', formatError(error));
+    });
+
+    requestFreegeoip(ip, function (data) {
+            appendLine('Месторасположение (freegeoip.net)', formatGeoipData(data));
+    }, function (error) {
+        appendLine('Месторасположение (freegeoip.net)', formatError(error));
+    });
+})();
