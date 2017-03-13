b44fa9c784d465ed4bb048179cf2ad6450c2a442
Alex P <alex.pantec@gmail.com>
2017-03-13 20:21:35 +0200

    quick move thread

--- pf_fast_thread_management.user.js
+++ pf_fast_thread_management.user.js
@@ -13,24 +13,81 @@
 (function() {
     'use strict';
 
+    if (window.fastThreadManagement)
+        return;
+    window.fastThreadManagement = true;
+
     function addStyle(css) {
         $(`<style>${css}</style>`).appendTo('head');
     }
 
     const adminRadioButtons = $('.vbmenu_option div label[for*="ao_"]:has(input[type="radio"])');
 
+    const adminMenu = adminRadioButtons.eq(0).closest('.vbmenu_option');
+
     adminRadioButtons.children().hide();
 
-    adminRadioButtons.css({
-        "cursor": "pointer"
-    });
-
-    adminRadioButtons.eq(0).closest('.vbmenu_option').css({
-        "cursor": "default",
-        "padding": 0
-    });
-
     adminRadioButtons.eq(0).closest('tr').next(':has(input.button)').hide();
+
+    adminRadioButtons.parent().addClass('admin-menu-item');
+
+    adminRadioButtons.parent().click(function () {
+        $(this).find('input[type="radio"]').prop("checked", true);
+        $(this).closest('form')[0].submit();
+    });
+
+    function getThreadTitle() {
+        return $.trim($('td:has(a[href*="' + location.search + '"]) strong')[0].innerText.trim());
+    }
+
+    function getThreadId() {
+        return $("#threadsearch_menu").find("a")[1].href.split("=")[1];
+    }
+
+    function moveThread(destForumId) {
+        $(`<form action="postings.php?do=domovethread&amp;t=${getThreadId()}" method="post" name="vbform">
+                <input type="hidden" name="s" value="">
+                <input type="hidden" name="securitytoken" value="${window.SECURITYTOKEN}">
+                <input type="hidden" name="t" value="${getThreadId()}">
+                <input type="hidden" name="destforumid" value="${destForumId}">
+                <input type="hidden" name="do" value="domovethread">
+                <input type="hidden" name="title" value="${getThreadTitle()}">
+                <input type="hidden" name="redirect" value="expires">
+                <input type="hidden" name="period" value="1">
+                <input type="hidden" name="frame" value="d">
+                <input type="hidden" name="redirecttitle" value="${getThreadTitle()}">
+            </form>`)
+            .appendTo('body')
+            .submit();
+    }
+
+    function loadingIndicatorHtml() {
+        return '<img src="/images/1070/misc/progress.gif"/>';
+    }
+
+    const quickMoveForums = [
+        { id: 31, buttonText: 'Переместить в Помощь студентам' }
+    ];
+
+    $.each(quickMoveForums, function () {
+        $(`<div class="admin-menu-item"><label>${this.buttonText}</label></div>`)
+            .appendTo(adminMenu)
+            .click(function () {
+                $(loadingIndicatorHtml()).insertAfter($(this));
+                moveThread(this.id);
+            });
+    });
+
+    function setMenuStyle() {
+        $(".admin-menu-item").children().css({
+            "cursor": "pointer"
+        });
+
+        adminMenu.css({
+            "cursor": "default",
+            "padding": 0
+        });
+    }
 
     addStyle(`
         .admin-menu-item {
@@ -43,10 +100,6 @@
             background: #ffffcc;
             color: #000000;
         }`);
-    adminRadioButtons.parent().addClass('admin-menu-item');
 
-    adminRadioButtons.parent().click(function () {
-        $(this).find('input[type="radio"]').prop("checked", true);
-        $(this).closest('form')[0].submit();
-    });
+    setMenuStyle();
 })();
