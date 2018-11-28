770159f6caff991f8853ba3a042f8c32380f4c46
Alex P <alex.pantec@gmail.com>
2018-11-28 15:39:18 +0200

    handle quick edits

--- pf_hotkey.user.js
+++ pf_hotkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ProgrammersForumHotkey
 // @namespace    http://programmersforum.ru/
-// @version      0.2
+// @version      0.3
 // @description  allows to submit posts with Ctrl+Enter
 // @author       Alex P
 // @include      *programmersforum.ru/*
@@ -12,13 +12,40 @@
 (function () {
     'use strict';
 
-    $('textarea').keydown(function (e) {
+    function ctrlEnterPressed(keyEcent) {
+        return (keyEcent.keyCode === 10 || keyEcent.keyCode === 13) && keyEcent.ctrlKey;
+    }
 
-        // Ctrl-Enter pressed
-        if ((e.keyCode === 10 || e.keyCode === 13) && e.ctrlKey) {
+    function isQuickEdit(textarea) {
+        return textarea.attr('id').indexOf('_QE_') > 0;
+    }
+
+    const textAreaKeyHandler = function (e) {
+        if (ctrlEnterPressed(e)) {
             const textarea = $(this);
-            textarea.closest('form').submit();
+
+            if (isQuickEdit(textarea)) {
+                textarea.closest('table').find('input[id$="_save"]').click();
+            } else {
+                textarea.closest('form').submit();
+            }
+
             textarea.blur();
         }
-    });
+    };
+
+    $('textarea').keydown(textAreaKeyHandler);
+
+    if (typeof MutationObserver !== 'undefined') {
+        const posts = $('#posts');
+        if (posts.length) {
+            new MutationObserver(function (mutations) {
+                mutations.forEach(function (mutation) {
+                    if (mutation.addedNodes) {
+                        $(mutation.addedNodes).find('textarea').keydown(textAreaKeyHandler);
+                    }
+                });
+            }).observe(posts[0], {childList: true, subtree: true});
+        }
+    }
 })();
