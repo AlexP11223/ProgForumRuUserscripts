b58e0aa92108e5e8ae45fb26e6ee8d61cb1ddb5f
Alex P <alex.pantec@gmail.com>
2018-12-09 22:31:14 +0200

    browser, os icons

--- pf_geoip.user.js
+++ pf_geoip.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ProgrammersForumGeoIp
 // @namespace    http://programmersforum.ru/
-// @version      0.6
+// @version      0.7
 // @description  adds country/city info on the page with user IP, as well as current user agent, IP for online user
 // @author       Alex P
 // @include      http://programmersforum.ru/postings.php?do=getip*
@@ -142,6 +142,129 @@
         });
     }
 
+    function getOsIcon(uaData) {
+        const win8Icon = 'https://i.imgur.com/OC1xkLD.png';
+        const winIcon = 'https://i.imgur.com/o08ewuG.png';
+        const linuxIcon = 'https://i.imgur.com/I1JBl7R.png';
+        const appleIcon = 'https://i.imgur.com/m8Iw72B.png';
+        const androidIcon = 'https://i.imgur.com/Pmi5K2W.png';
+
+        switch (uaData.os.name) {
+            case 'Windows':
+            case 'Windows Server':
+            case 'Windows Phone':
+                switch (uaData.os.version) {
+                    case '10':
+                    case '8':
+                    case '8.1':
+                    case '2012':
+                    case '2016':
+                        return win8Icon;
+                    default:
+                        return winIcon;
+                }
+            case 'Linux':
+            case 'Unix':
+            case 'Ubuntu':
+            case 'Mint':
+            case 'Arch':
+            case 'Gentoo':
+            case 'RedHat':
+            case 'SUSE':
+                return linuxIcon;
+            case 'Mac OS':
+            case 'iOS':
+                return appleIcon;
+            case 'Android':
+                return androidIcon;
+            default:
+                if (uaData.os.name.indexOf('Windows') > -1)
+                    return winIcon;
+                if (uaData.os.name.indexOf('Linux') > -1 || uaData.os.name.indexOf('BSD') || uaData.os.name.indexOf('GNU'))
+                    return linuxIcon;
+                return null;
+        }
+    }
+
+    function getDeviceIcon(uaData) {
+        switch (uaData.os.name) {
+            case 'iOS':
+            case 'Android':
+            case 'Windows Phone':
+            case 'Windows Mobile':
+            case 'IEMobile':
+                return 'https://i.imgur.com/0bSHkRs.png';
+            default:
+                return null;
+        }
+    }
+
+    function getBrowserIcon(uaData) {
+        // some user agents are truncated
+        function isYandexBrowser(ua) {
+            return ua.split(' ').pop().indexOf('Ya') > -1;
+        }
+
+        function isChrome(ua) {
+            return ua.split(' ').pop().indexOf('Ch') > -1;
+        }
+
+        function getIconId() {
+            switch (uaData.browser.name.toLowerCase()) {
+                case 'firefox':
+                case 'iceweasel':
+                case 'icecat':
+                case 'icedragon':
+                case 'iceape':
+                    return 'firefox/firefox';
+                case 'chrome':
+                case 'chrome webview':
+                case 'chrome headless':
+                case 'chromium':
+                case 'comodo dragon':
+                    if (isYandexBrowser(uaData.ua))
+                        return 'yandex/yandex';
+                    return 'chrome/chrome';
+                case 'yandex':
+                    return 'yandex/yandex';
+                case 'safari':
+                    return 'safari/safari';
+                case 'opera':
+                case 'opera mini':
+                case 'opera tablet':
+                case 'opera mobi':
+                case 'opera coast':
+                    return 'opera/opera';
+                case 'vivaldi':
+                    return 'vivaldi/vivaldi';
+                case '2345explorer':
+                    return 'archive/internet-explorer_6/internet-explorer_6';
+                case 'iemobile':
+                case 'ie':
+                    if (!uaData.browser.version)
+                        return 'archive/internet-explorer_6/internet-explorer_6';
+                    if (uaData.browser.version[0] === '7' || uaData.browser.version[0] === '8')
+                        return 'archive/internet-explorer_7-8/internet-explorer_7-8';
+                    return 'archive/internet-explorer_9-11/internet-explorer_9-11';
+                case 'edge':
+                    return 'edge/edge';
+                default:
+                    if (isChrome(uaData.ua))
+                        return 'chrome/chrome';
+                    return null;
+            }
+        }
+
+        const id = getIconId();
+        return id ? `https://cdnjs.cloudflare.com/ajax/libs/browser-logos/46.1.0/${id}_32x32.png` : null;
+    }
+
+    function getImgHtml(url, width, height) {
+        if (!url)
+            return '';
+        return `<img src="${url}" height="${width}" width="${height}"/>`
+    }
+
     const ipElement = window.document.querySelector('.panelsurround div.panel div div strong');
     if (!ipElement)
         return;
@@ -182,7 +305,8 @@
         appendLine(onlineUserInfoContainer, 'User-Agent', '<span id="parsedUa"></span>' + userAgent);
         parseUserAgent(userAgent, function (uaData) {
             const container = $('#parsedUa');
-            container.html(`${uaData.os.name} ${uaData.os.version}, ${uaData.browser.name} ${uaData.browser.version}<br/>`);
+            container.html(`${getImgHtml(getDeviceIcon(uaData), 16, 16)} ${getImgHtml(getOsIcon(uaData), 16, 16)} ${uaData.os.name} ${uaData.os.version},
+                            ${getImgHtml(getBrowserIcon(uaData), 16, 16)} ${uaData.browser.name} ${uaData.browser.version}<br/>`);
         });
         appendLine(onlineUserInfoContainer, 'IP адрес', ip);
         if (host) {
