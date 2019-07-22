747ab6183dc0c3467fb5043423cb7aba5467d919
Alex P <alex.pantec@gmail.com>
2019-07-22 21:43:13 +0300

    reduce page jumping

--- pf_declutter.user.js
+++ pf_declutter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ProgrammersForum Hide logo, etc.
 // @namespace    programmersforum.ru
-// @version      1.3
+// @version      1.4
 // @description  hides the huge logo and other useless stuff
 // @author       Alex P
 // @include      *programmersforum.ru/*
@@ -25,6 +25,7 @@
                    #header_right_cell,
                    div.page > div > b,
                    div.page > div > b + br,
+                   div.page > div > b + br + br,
                    div.page > div > center,
                    div.page > div > center + br
         { display: none; }`);
